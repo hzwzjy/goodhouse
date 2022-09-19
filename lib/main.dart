db199d66c2d8741821d2e455ff748133ec2abc96
LAPTOP-CA8M740A\hzwjxc <542658397@qq.com>
2022-09-19 21:53:43 +0800

    sentry 平台错误收集

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,28 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:goodhouse/application.dart';
 import 'package:goodhouse/common/provider/app.dart';
 import 'package:goodhouse/global.dart';
 import 'package:provider/provider.dart';
+import 'package:sentry/sentry.dart';
 
-void main() async {
+Future<void> main() async {
+  runZonedGuarded(() async {
+    await Sentry.init(
+      (options) {
+        options.dsn =
+            'https://564fda5fe0024a819905efd82d727274@o1381110.ingest.sentry.io/6760123';
+      },
+    );
+
+    initApp();
+  }, (Object exception, StackTrace stackTrace) async {
+    await Sentry.captureException(exception, stackTrace: stackTrace);
+  });
+}
+
+void initApp() async {
   await Global.init();
   runApp(
     MultiProvider(
